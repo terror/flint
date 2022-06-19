c0d4723b448ae91cb802e0b78429b2b4ba063750
Liam <liam@scalzulli.com>
2022-06-19 17:59:52 -0400

    Cleanup arguments

--- src/arguments.rs
+++ src/arguments.rs
@@ -15,27 +15,29 @@ impl Arguments {
           .guess()?
           .ok_or("Unsupported file type")?;
 
-        let checkers = Config::load()?
+        let sources = Config::load()?
           .checkers
           .iter()
-          .map(|path| {
-            let source = &fs::read_to_string(&path.expand()).unwrap();
-            serde_yaml::from_str::<Checker>(source).unwrap()
-          })
+          .map(|path| fs::read_to_string(&path.expand()))
+          .collect::<Result<Vec<_>, _>>()?;
+
+        let checkers = sources
+          .iter()
+          .map(|source| serde_yaml::from_str::<Checker>(source))
+          .collect::<Result<Vec<Checker>, _>>()?;
+
+        checkers
+          .iter()
           .filter(|checker| checker.language == language)
-          .collect::<Vec<Checker>>();
-
-        checkers.iter().try_for_each(|checker| {
-          let mut parser = Parser::new(checker.language.clone())?;
-
-          checker.rules.iter().try_for_each(|rule| {
-            parser.query(QueryConfig {
-              name: rule.0,
-              path: path.clone(),
-              rule: rule.1.clone(),
+          .try_for_each(|checker| {
+            checker.rules.iter().try_for_each(|rule| {
+              Parser::new(checker.language.clone())?.query(QueryConfig {
+                name: rule.0,
+                path: path.clone(),
+                rule: rule.1.clone(),
+              })
             })
           })
-        })
       })
   }
 }
